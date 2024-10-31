7cf0513ce839a10baa55581bb2c3a423d2863d2b
Mahmoud Kamal <213179+mahmoudkamalx17@users.noreply.github.com>
2024-10-31 12:34:55 +0300

    Setup Stripe Payment in Andorid

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:payment_getaway/app.dart';
 import 'package:payment_getaway/core/routes/app_router.dart';
 
 void main() async {
-  // Stripe.publishableKey = stripePublishableKey;
-
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(PaymentGateway(appRouter: AppRouter()));
 }
